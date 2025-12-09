7378883b76b4f9fbf3350ba5fbfb02aef5184c9a
Lukas Rasche <lukas.rasche.2002@icloud.com>
2025-12-09 23:41:15 +0100

    Added mini sectors for: Brazil, Monaco, Japan, Belgium, USA

--- backend/utils/sectors.py
+++ backend/utils/sectors.py
@@ -4,8 +4,94 @@ sector_dict = {
     'Italian Grand Prix': [0,800,1050,2050,2300,2700,2950,3850,4200,5000,5500],
     'Austrian Grand Prix': [0,330,500,1300,1450,2100,2250,2600,3100,3600,4020],
     'Spanish Grand Prix': [0,750,1250,1600,1870,2000,2200,2450,2600,2800,3000,3370,3900,4400],
-    'Bahrain Grand Prix': [0,600,800,1400,1600,1800,2150,2300,2600,2750,3350,3900,4200,4790,5000],
-    'Abu Dhabi Grand Prix': [0,275,700,1300,1520,2550,2750,3550,3850,4250,4375,4700,5100]
+    'Bahrain Grand Prix': [0,600,850,1450,1600,1800,2150,2300,2600,2750,3350,3900,4200,4790,5000],
+    'Abu Dhabi Grand Prix': [0,275,700,1300,1520,2550,2750,3550,3850,4250,4375,4700,5100],
+    'Japanese Grand Prix': [0,700,900,2000,2200,2350,2450,2800,3000,3700,3975,5300,5600,5800],
+    'São Paulo Grand Prix': [0,300,550,1300,1470,1875,2200,2500,2750,3100,3270,4309],
+    'Belgian Grand Prix': [0,270,450,950,1320,2330,2700,2900,3200,3350,3720,4050,4350,5300,6600,6820,7004],
+    'Monaco Grand Prix': [0,150,240,650,820,910,1050,1450,2000,2130,2300,2600,2775,3000,3337],
+    "United States Grand Prix": [0,450,700,1100,1450,2020,2460,2700,3600,3800,4350,4850,5515]
+}
+
+labels_USA = {
+    1: 'Straight',
+    2: 'Slow',
+    3: 'Straight',
+    4: 'Fast',
+    5: 'Medium',
+    6: 'Straight',
+    7: 'Slow',
+    8: 'Straight',
+    9: 'Slow',
+    10: 'Medium',
+    11: 'Fast',
+    12: 'Medium'
+}
+
+labels_monaco = {
+    1: 'Straight',
+    2: 'Medium',
+    3: 'Straight',
+    4: 'Fast',
+    5: 'Medium',
+    6: 'Straight',
+    7: 'Slow',
+    8: 'Straight',
+    9: 'Slow',
+    10: 'Straight',
+    11: 'Fast',
+    12: 'Medium',
+    13: 'Slow',
+    14: 'Straight'
+}
+
+labels_belgium = {
+    1: 'Straight',
+    2: 'Slow',
+    3: 'Straight',
+    4: 'Fast',
+    5: 'Straight',
+    6: 'Medium',
+    7: 'Straight',
+    8: 'Slow',
+    9: 'Medium',
+    10: 'Straight',
+    11: 'Fast',
+    12: 'Straight',
+    13: 'Medium',
+    14: 'Straight',
+    15: 'Slow',
+    16: 'Straight'
+}
+
+labels_brazil = {
+    1: 'Straight',
+    2: 'Medium',
+    3: 'Straight',
+    4: 'Medium',
+    5: 'Straight',
+    6: 'Fast',
+    7: 'Slow',
+    8: 'Slow',
+    9: 'Fast',
+    10: 'Medium',
+    11: 'Straight'
+}
+
+labels_japan = {
+    1: "Straight",
+    2: "Medium",
+    3: "Fast",
+    4: "Straight",
+    5: "Fast",
+    6: "Medium",
+    7: "Straight",
+    8: "Slow",
+    9: "Straight",
+    10: "Medium",
+    11: "Straight",
+    12: "Slow",
+    13: "Straight"
 }
 
 labels_spanish = {
@@ -110,5 +196,10 @@ label_dict = {
     'Bahrain Grand Prix': labels_bahrain,
     'Abu Dhabi Grand Prix': labels_abuDhabi,
     'Italian Grand Prix': labels_italy,
-    'Austrian Grand Prix': labels_austria
+    'Austrian Grand Prix': labels_austria,
+    'Japanese Grand Prix': labels_japan,
+    "São Paulo Grand Prix": labels_brazil,
+    "Belgian Grand Prix": labels_belgium,
+    "Monaco Grand Prix": labels_monaco,
+    "United States Grand Prix": labels_USA
 }
